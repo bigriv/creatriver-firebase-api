5f9840bae1168b4d38476379525fdc95d9509b9f
bigriv <bigriv1344@gmail.com>
2024-10-20 01:34:34 +0900

    add: Wasの会話メッセージ登録API作成

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,19 +1,50 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+import * as express from "express";
+import { JsonUtils } from "./utils/json";
+import { isWasTalkDefine } from "./formats/games/was/talk";
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+admin.initializeApp();
+const app = express();
+const bucket = admin.storage().bucket();
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+app.post("/edits/games/was/talk", async (req, res) => {
+  try {
+    const json = req.body;
+    if (!json) {
+      res.status(400).send("Bad Request");
+      return;
+    }
+    if (!JsonUtils.isKeyValue(json)) {
+      res
+        .status(400)
+        .send("Bad Request. Request body is not key-value format.");
+      return;
+    }
+    if (!Object.keys(json).every((key) => isWasTalkDefine(json[key]))) {
+      res.status(400).send("Bad Request. The talk is not talk format.");
+      return;
+    }
+    const filePath = "gameofus/games/was";
+    const fileName = "talk.json";
+    const fileRef = bucket.file(`${filePath}/${fileName}`);
+    const [exists] = await fileRef.exists();
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+    // ファイルが既に存在する場合はバックアップを作成する
+    if (exists) {
+      const backupFilePath = `${filePath}/backups/talks`;
+      const backFileName = `talk-${Date.now()}.json`;
+      const backupFileRef = bucket.file(`${backupFilePath}/${backFileName}`);
+
+      await fileRef.copy(backupFileRef);
+    }
+
+    await fileRef.save(JSON.stringify(json));
+    res.status(200).send("Success");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Unexpected Error");
+  }
+});
+
+export const api = functions.https.onRequest(app);
