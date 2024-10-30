2eb7c662d6bb858ada109e6cce32a04ae90c0f09
bigriv <bigriv1344@gmail.com>
2024-10-31 00:18:49 +0900

    update: DIするように構成を改善

---
+++ functions/src/controller/edits/games/was/talk.ts
@@ -0,0 +1,63 @@
+import { Request, Response } from "express";
+import { KeyValue } from "@/utils/json";
+import { isWasTalkDefine, WasTalkDefine } from "@/formats/games/was/talk";
+
+export interface WasTalkRepositoryIf {
+  findAll(): Promise<KeyValue<WasTalkDefine>>;
+  save(talk: WasTalkDefine): Promise<void>;
+  deleteById(id: string): Promise<void>;
+}
+
+export class EditWasTalkController {
+  private repository: WasTalkRepositoryIf;
+
+  constructor(repository: WasTalkRepositoryIf) {
+    this.repository = repository;
+  }
+
+  /**
+   * GETメソッド
+   * @param _req リクエスト
+   * @param res レスポンス
+   */
+  async get(_req: Request, res: Response) {
+    try {
+      const contents = await this.repository.findAll();
+      res.status(200).send(contents);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send("Unexpected Error");
+    }
+  }
+
+  /**
+   * POSTメソッド
+   * @param req リクエスト
+   * @param res レスポンス
+   */
+  async post(req: Request, res: Response) {
+    try {
+      const json = req.body;
+      if (!isWasTalkDefine(json)) {
+        res.status(400).send("Bad Request. The talk is not talk format.");
+        return;
+      }
+
+      await this.repository.save(json);
+      res.status(200).send("Success");
+    } catch (error) {
+      console.error(error);
+      res.status(500).send("Unexpected Error");
+    }
+  }
+
+  /**
+   * DELETEメソッド
+   * @param req リクエスト
+   * @param res レスポンス
+   */
+  async delete(req: Request, res: Response) {
+    await this.repository.deleteById(req.params.id);
+    res.status(200).send("Success");
+  }
+}
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,9 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 import * as express from "express";
-import { JsonUtils } from "./utils/json";
-import { isWasTalkDefine } from "./formats/games/was/talk";
-import { initLocalStorage } from "./local";
+import { initLocalStorage } from "@/local";
+import { EditWasTalkController } from "@/controller/edits/games/was/talk";
+import { WasTalkRepository } from "@/repositories/games/was/talk";
 
 admin.initializeApp();
 
@@ -13,91 +13,11 @@ const bucket = admin.storage().bucket();
 // TODO: ローカル起動の場合のみ実行するように修正する
 initLocalStorage(bucket);
 
-app.get("/edits/games/was/talk", async (req, res) => {
-  try {
-    const filePath = "gameofus/games/was";
-    const fileName = "talk.json";
-    const fileRef = bucket.file(`${filePath}/${fileName}`);
-    const [exists] = await fileRef.exists();
+const editWasTalkController = new EditWasTalkController(
+  new WasTalkRepository(bucket)
+);
+app.get("/edits/games/was/talk", editWasTalkController.get);
+app.post("/edits/games/was/talk", editWasTalkController.post);
+app.delete("/edits/games/was/talk/:id", editWasTalkController.delete);
 
-    if (!exists) {
-      throw new Error("File is not exist.");
-    }
-
-    const [fileContents] = await fileRef.download();
-    const json = JSON.parse(fileContents.toString());
-    if (!JsonUtils.isKeyValue(json)) {
-      throw new Error("File is broken. There was not key-value format.");
-    }
-    if (!Object.keys(json).every((key) => isWasTalkDefine(json[key]))) {
-      throw new Error("File is broken. It has not talk define format.");
-    }
-
-    res.status(200).send(JSON.parse(fileContents.toString()));
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Unexpected Error");
-  }
-});
-
-app.post("/edits/games/was/talk", async (req, res) => {
-  try {
-    const json = req.body;
-    if (!isWasTalkDefine(json)) {
-      res.status(400).send("Bad Request. The talk is not talk format.");
-      return;
-    }
-    const filePath = "gameofus/games/was";
-    const fileName = "talk.json";
-    const fileRef = bucket.file(`${filePath}/${fileName}`);
-    const [exists] = await fileRef.exists();
-
-    let putData = json;
-    // ファイルが既に存在する場合はバックアップを作成する
-    if (exists) {
-      const backupFilePath = `${filePath}/backups/talks`;
-      const backFileName = `talk-${Date.now()}.json`;
-      const backupFileRef = bucket.file(`${backupFilePath}/${backFileName}`);
-
-      await fileRef.copy(backupFileRef);
-
-      const [fileContents] = await fileRef.download();
-      const storageData = JSON.parse(fileContents.toString());
-      putData = Object.assign(storageData, { [putData.id]: putData });
-    }
-
-    // 既存のファイルと結合して保存
-    await fileRef.save(JSON.stringify(putData));
-    res.status(200).send("Success");
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Unexpected Error");
-  }
-});
-
-app.delete("/edits/games/was/talk/:id", async (req, res) => {
-  const filePath = "gameofus/games/was";
-  const fileName = "talk.json";
-  const fileRef = bucket.file(`${filePath}/${fileName}`);
-  const [exists] = await fileRef.exists();
-
-  // ファイルが存在しない場合はエラー
-  if (!exists) {
-    throw new Error("File is not exist.");
-  }
-
-  const backupFilePath = `${filePath}/backups/talks`;
-  const backFileName = `talk-${Date.now()}.json`;
-  const backupFileRef = bucket.file(`${backupFilePath}/${backFileName}`);
-
-  await fileRef.copy(backupFileRef);
-
-  const [fileContents] = await fileRef.download();
-  const storageData = JSON.parse(fileContents.toString());
-  delete storageData[req.params.id];
-
-  // 既存のファイルを上書きして保存
-  await fileRef.save(JSON.stringify(storageData));
-  res.status(200).send("Success");
-});
 export const api = functions.https.onRequest(app);
