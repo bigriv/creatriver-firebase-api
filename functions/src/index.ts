253414e52f4a7758a6880a7117c157130317fceb
bigriv <bigriv1344@gmail.com>
2024-11-02 09:58:01 +0900

    modify: 正常に起動しない問題とリクエスト・レスポンスが正しく処理されない問題を解消

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,9 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 import * as express from "express";
+
+require('module-alias/register');
+
 import { initLocalStorage } from "@/local";
 import { EditWasTalkController } from "@/controller/edits/games/was/talk";
 import { EditWasBattleController } from "@/controller/edits/games/was/battle";
@@ -18,15 +21,15 @@ initLocalStorage(bucket);
 const editWasTalkController = new EditWasTalkController(
   new WasTalkRepository(bucket)
 );
-app.get("/edits/games/was/talk", editWasTalkController.get);
-app.post("/edits/games/was/talk", editWasTalkController.post);
-app.delete("/edits/games/was/talk/:id", editWasTalkController.delete);
+app.get("/edits/games/was/talk", (req, res) => editWasTalkController.get(req, res));
+app.post("/edits/games/was/talk", (req, res) => editWasTalkController.post(req, res));
+app.delete("/edits/games/was/talk/:id", (req, res) => editWasTalkController.delete(req, res));
 
 const editWasBattleController = new EditWasBattleController(
   new WasBattleRepository(bucket)
 );
-app.get("/edits/games/was/battle", editWasBattleController.get);
-app.post("/edits/games/was/battle", editWasBattleController.post);
-app.delete("/edits/games/was/battle/:id", editWasBattleController.delete);
+app.get("/edits/games/was/battle", (req, res) => editWasBattleController.get(req, res));
+app.post("/edits/games/was/battle", (req, res) => editWasBattleController.post(req, res));
+app.delete("/edits/games/was/battle/:id", (req, res) => editWasBattleController.delete(req, res));
 
 export const api = functions.https.onRequest(app);
